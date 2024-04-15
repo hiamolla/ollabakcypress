330f2cefb55373ea49564401f5f43dc0c8544a68
hiamolla <ollaville@yahoo.com>
2024-04-15 18:22:25 +0100

    ollabakcypress

--- cypress/e2e/tninjafp.spec.cy.js
+++ cypress/e2e/tninjafp.spec.cy.js
@@ -7,11 +7,13 @@ describe('example to-do app', () => {
     it('Verify the user is able to reset password', () => {
         cy.contains('My Account').click()
         cy.contains('Login').click()
-        cy.get('#input-email').type('codelanguage@gmail.com')
-        cy.get('#input-password').type('@olabak')
+        cy.get('#input-email').should('be.visible').type('codelanguage@gmail.com')
+        cy.get('#input-password').should('be.visible').type('@olabak')
         cy.contains('Forgotten Password').click()
-        cy.get('#input-email').type('codelanguage101@gmail.com')
+        cy.get('#input-email').should('be.visible').type('codelanguage101@gmail.com')
         cy.get('[value="Continue"]').click()
         cy.get('.alert').should('have.text', "An email with a confirmation link has been sent your email address.")
     }) 
 }) 
+
+
--- cypress/e2e/tninjalogin.spec.cy.js
+++ cypress/e2e/tninjalogin.spec.cy.js
@@ -7,16 +7,16 @@ describe('example to-do app', () => {
     it('Verify the user is able to login with valid credentials', () => {
         cy.contains('My Account').click()
         cy.contains('Login').click()
-        cy.get('#input-email').type('codelanguage@gmail.com')
-        cy.get('#input-password').type('@olabak0805')
+        cy.get('#input-email').should('be.visible').type('codelanguage101@gmail.com')
+        cy.get('#input-password').should('be.visible').type('@olabak0805')
         cy.get('[value="Login"]').click()
     })
 
     it('Verify the user is unable to login with invalid credentials', () => {
         cy.contains('My Account').click()
         cy.contains('Login').click()
-        cy.get('#input-email').type('codelanguage@gmail.com')
-        cy.get('#input-password').type('@olabak')
+        cy.get('#input-email').should('be.visible').type('codelanguage101@gmail.com')
+        cy.get('#input-password').should('be.visible').type('@olabak')
         cy.get('[value="Login"]').click()
         cy.get('.alert').should('have.text', "Warning: No match for E-Mail Address and/or Password.")
     })
