bee4d747c8b6fa072d171b5449c2a02abca0a51f
hiamolla <ollaville@yahoo.com>
2024-04-20 10:46:30 +0100

    ollabakcypress

--- cypress/e2e/tninjafp.spec.cy.js
+++ cypress/e2e/tninjafp.spec.cy.js
@@ -7,10 +7,10 @@ describe('example to-do app', () => {
     it('Verify the user is able to reset password', () => {
         cy.contains('My Account').click()
         cy.contains('Login').click()
-        cy.get('#input-email').should('be.visible').type('codelanguage@gmail.com')
-        cy.get('#input-password').should('be.visible').type('@olabak')
+        cy.get('#input-email').should('exist').should('be.visible').type('codelanguage@gmail.com')
+        cy.get('#input-password').should('exist').should('be.visible').type('@olabak')
         cy.contains('Forgotten Password').click()
-        cy.get('#input-email').should('be.visible').type('codelanguage101@gmail.com')
+        cy.get('#input-email').should('exist').should('be.visible').type('codelanguage101@gmail.com')
         cy.get('[value="Continue"]').click()
         cy.get('.alert').should('have.text', "An email with a confirmation link has been sent your email address.")
     }) 
--- cypress/e2e/tninjasearch.spec.cy.js
+++ cypress/e2e/tninjasearch.spec.cy.js
@@ -5,12 +5,12 @@ describe('example to-do app', () => {
     })
 
     it('Verify the user is able to search for an item available on the website', () => {
-        cy.get("[placeholder='Search']").type("MacBook");
+        cy.get("[placeholder='Search']").should('exist').should('be.visible').type("MacBook");
         cy.get('.input-group-btn > .btn > .fa').click()
     })
 
     it('Verify the user is unable to search for an item not available on the website', () => {
-        cy.get("[placeholder='Search']").type("Shoes");
+        cy.get("[placeholder='Search']").should('exist').should('be.visible').type("Shoes");
         cy.get('.input-group-btn > .btn > .fa').click()
         cy.get('#content > h2').should('have.text', "Products meeting the search criteria")
         cy.get('#content > :nth-child(7)').should('have.text', "There is no product that matches the search criteria.")
